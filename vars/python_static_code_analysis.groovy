5eb76871defc71c69ef0f9bfb12fc62d7ec3c534
Jogeswar98 <jogeswarpradhan98@gmail.com>
2025-03-17 05:08:10 +0530

    Update python_static_code_analysis.groovy

--- vars/python_static_code_analysis.groovy
+++ vars/python_static_code_analysis.groovy
@@ -1,1 +1,13 @@
-
+def call(String sonarServerName) {
+    withSonarQubeEnv(sonarServerName) {
+        def scannerHome = tool name: 'Sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=attendance \
+            -Dsonar.projectName=attendance-api \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.issue.types=CODE_SMELL \
+            -Dsonar.report.export.path=target/static-code-analysis-report.json
+        """
+    }
+}
