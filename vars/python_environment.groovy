fa24a189ac7d965694f0ad7f46fd8153ad0ca467
Jogeswar98 <jogeswarpradhan98@gmail.com>
2025-03-17 05:07:09 +0530

    Update python_environment.groovy

--- vars/python_environment.groovy
+++ vars/python_environment.groovy
@@ -1,1 +1,9 @@
-
+def call() {
+    sh """
+        sudo apt-get update -y
+        sudo apt-get install -y python3.11 python3.11-venv python3-pip
+        python3.11 -m venv venv
+        . venv/bin/activate
+        pip install --upgrade pip poetry
+    """
+}
